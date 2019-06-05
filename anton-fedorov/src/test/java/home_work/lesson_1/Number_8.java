c40a0d96aea885798bc005c8549533a6927e4867
Anton <feodan@yandex.ru>
2019-06-05 11:01:50 +0300

    fixes

---
+++ anton-fedorov/src/test/java/home_work/lesson_1/Number_8.java
@@ -0,0 +1,18 @@
+package home_work.lesson_1;
+
+import java.util.Scanner;
+
+public class Number_8 {
+    /**
+     * 8. Создать класс, который принимает число с плавающей точкой, введенное пользователем в консоль, и выводит его в виде целого числа и экспоненты.
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input number:");
+        double number = scanner.nextDouble();
+
+        String[] arr = String.valueOf(number).split("\\.");
+        System.out.println("Integer: " + arr[0]);
+        System.out.println("Exhibitor: " + arr[1]);
+    }
+}
