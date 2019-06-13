a4426f5bba3b4e7b18553527a7239d3980c21d20
Sergey Smolenkov <tifon27@gmail.com>
2019-06-14 00:47:05 +0300

    add Reactangle

--- smolenkov-sergey/src/main/java/homework/lesson4/Rectangle.java
+++ smolenkov-sergey/src/main/java/homework/lesson4/Rectangle.java
@@ -1,4 +1,57 @@
 package homework.lesson4;
 
 public class Rectangle {
+
+    private double length = 1.0;
+    private double width = 1.0;
+    private String color = "red";
+    private double perimeter = 2 * (length + width);
+    private double area = length * width;
+
+    public Rectangle() {
+    }
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public Rectangle(double length, double width, String color) {
+        this.length = length;
+        this.width = width;
+        this.color = color;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public double getArea() {
+        return area;
+    }
+
+    public double getPerimeter() {
+        return perimeter;
+    }
+
 }
