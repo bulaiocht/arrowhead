eae4b0873c5dcd0d5492d7b703b8b92ca05eaf16
Sergey Smolenkov <tifon27@gmail.com>
2019-06-14 00:57:36 +0300

    add Point

--- smolenkov-sergey/src/main/java/homework/lesson4/Point.java
+++ smolenkov-sergey/src/main/java/homework/lesson4/Point.java
@@ -1,4 +1,39 @@
 package homework.lesson4;
 
 public class Point {
+
+    private double x = 1.0;
+    private double y = 1.0;
+
+    public Point() {
+    }
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double distance(double x, double y){
+        return distance(x, y);
+    }
+
+    public double distance(double point){
+        return distance(point);
+    }
 }
