af274bd073923087379fe473465116174687deaf
Kocherga Vitalii <ko4erha@gmail.com>
2019-06-14 21:29:50 +0300

    (feature) exceptions

--- examples/src/main/java/exceptions/BallCatcher.java
+++ examples/src/main/java/exceptions/BallCatcher.java
@@ -1,4 +1,35 @@
 package exceptions;
 
+import java.io.IOException;
+
 public class BallCatcher {
+
+    public static void main(String[] args) {
+
+        int counter = 0;
+
+        while (true) {
+
+            if (counter % 4 == 3 ) {
+
+                try {
+
+                    throw new Ball();
+
+                } catch (Ball ball) {
+
+                    System.out.println("Ball caught");
+                    counter = 0;
+
+                } catch (IllegalArgumentException e ) {
+
+                }
+            }
+
+            counter++;
+
+        }
+
+    }
+
 }
