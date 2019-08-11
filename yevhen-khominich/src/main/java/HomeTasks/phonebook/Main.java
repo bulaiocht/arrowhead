f6bbd27ab4bbfa2914c98089df78d4e853b98a76
Yevhen Khominich <zhekaukr328@gmail.com>
2019-08-11 15:05:36 +0300

    phohebook-version-1

---
+++ yevhen-khominich/src/main/java/HomeTasks/phonebook/Main.java
@@ -0,0 +1,9 @@
+package HomeTasks.phonebook;
+
+import HomeTasks.phonebook.views.ConsoleView;
+
+public class Main {
+    public static void main(String[] args) {
+        ConsoleView.displayMenu();
+    }
+}
